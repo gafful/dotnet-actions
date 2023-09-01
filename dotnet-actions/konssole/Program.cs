722c1af1c86613600f49c20fbb0254cb87fe0737
Godfred Afful <affulgodfred@gmail.com>
2023-09-01 15:38:37 +0000

    Added console project.

---
+++ dotnet-actions/konssole/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace Konssole
+{
+    internal static class Program
+    {
+        private static int Main(string[] args)
+        {
+            if (args[0] == "soup")
+                Console.WriteLine("not Not");
+
+            Console.WriteLine("Wee-here...!");
+            return 0;
+        }
+    }
+}
