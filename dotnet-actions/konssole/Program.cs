9d3c420653c45cefa55889a9e60d163fe339dd50
Godfred Afful <affulgodfred@gmail.com>
2023-09-06 12:35:43 +0000

    Added dependency and a GitHub dotnet action

--- dotnet-actions/konssole/Program.cs
+++ dotnet-actions/konssole/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using Newtonsoft.Json;
+
 namespace Konssole
 {
     internal static class Program
@@ -10,8 +12,21 @@ namespace Konssole
             if (args[0] == "soup")
                 Console.WriteLine("not Not");
 
-            Console.WriteLine("Wee-here...!");
+            Product product = new Product();
+            product.Name = "Goo Hul";
+            product.Expiry = new DateTime(2008, 12, 28);
+            product.Sizes = new string[] { "Small" };
+
+            string json = JsonConvert.SerializeObject(product);
+            Console.WriteLine($"Wee-here...!: {json}");
             return 0;
         }
+    }
+
+    internal class Product
+    {
+        public string Name { get; set; }
+        public DateTime Expiry { get; set; }
+        public string[] Sizes { get; set; }
     }
 }
