465228b622b1a2e0bb0ec1d09e888c85109dc8ab
Godfred Afful <affulgodfred@gmail.com>
2023-09-07 16:00:37 +0000

    Add project as external dependency

---
+++ Action.Common/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Action.Common
+{
+    public record Product
+    {
+        public string Name { get; set; }
+        public DateTime Expiry { get; set; }
+        public string[] Sizes { get; set; }
+    }
+}
